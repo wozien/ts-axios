57e9d89be783ce80ac3b21de9ffdd23919ee2c7b
wozien <571455773@qq.com>
2021-04-02 12:53:06 +0800

    feat: support response type pattern

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -5,10 +5,36 @@ import axios from '../../src'
 //   b: 2
 // }).then(res => console.log(res))
 
-axios('/base/post', {
-  method: 'post',
-  data: {
-    a: 1,
-    b: 2
+// axios('/base/post', {
+//   method: 'post',
+//   data: {
+//     a: 1,
+//     b: 2
+//   }
+// }).then(res => console.log(res))
+
+interface User {
+  name: string
+  age: number
+}
+
+interface ResponseData<T> {
+  code: number
+  msg: string
+  result: T
+}
+
+function getUser<T>() {
+  return axios.get<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(e => console.error(e))
+}
+
+async function test() {
+  const data = await getUser<User>()
+  if(data) {
+    console.log(data.result.name)
   }
-}).then(res => console.log(res))
+}
+
+test()
